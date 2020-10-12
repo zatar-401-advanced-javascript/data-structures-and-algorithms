34d21598175c4e19bb22739e8243e6f4deed463a
AbdulhakimZatar <hakimzatar@gmail.com>
2020-10-12 22:36:06 +0300

    challenege2

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,15 +1,15 @@
 'use strict';
 
-function reverseArray(arr){
-    let revArr = [];
-    let x = 0;
+function reverseArray(arr) {
+  let revArr = [];
+  let x = 0;
 
-    for (let i = arr.length-1; i >= 0; i--) {
-        revArr[x] = arr[i]
-        x++;
-    }
-    return revArr;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    revArr[x] = arr[i];
+    x++;
+  }
+  return revArr;
 }
 
-let array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
-console.log(reverseArray(array))
+let array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+console.log(reverseArray(array));
---
+++ challenges/arrayShift/README.md
@@ -0,0 +1,11 @@
+# Shift an Array
+Function take value and put it into mid of the new array.
+
+## Challenge
+Write a function called insertShiftArray which takes in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+
+## Approach & Efficiency
+First we need to find the mid of the array and approximate it to upper number, now we copy the array to new one, in begninig replace the mid to our value, then filling the numbers into the new array after the mid.
+
+## Solution
+![whiteboard](../../assets/shiftArray.jpg)
