0ab0291850ddc1da4c1d926401acc69f88542812
AbdulhakimZatar <hakimzatar@gmail.com>
2020-11-18 08:21:03 +0200

    challenge28

--- __tests__/quick-sort.test.js
+++ __tests__/quick-sort.test.js
@@ -1,17 +1,17 @@
 'use strict';
-const MergeSort = require('../challenges/merge-sort/merge-sort');
+const QuckSort = require('../challenges/quick-sort/quick-sort');
 
-describe('Merge Sort',()=>{
+describe('Quick Sort',()=>{
   it('Sort reverse-sorted ', () => {
-    const result = MergeSort([20,18,12,8,5,-2]);
+    const result = QuckSort([20,18,12,8,5,-2],0,5);
     expect(result).toEqual( [ -2, 5, 8, 12, 18, 20 ]);
   });
   it('Sort few uniques ', () => {
-    const result = MergeSort([5,12,7,5,5,7]);
+    const result = QuckSort([5,12,7,5,5,7],0,5);
     expect(result).toEqual([ 5, 5, 5, 7, 7, 12 ]);
   });
   it('Sort nearly-sorted ', () => {
-    const result = MergeSort([2,3,5,7,13,11]);
+    const result = QuckSort([2,3,5,7,13,11],0,5);
     expect(result).toEqual([ 2, 3, 5, 7, 11, 13 ]);
   });
 });
